0da844b2936ccf6d7c02d1807bf2686e093b0427
Michael Rhoads <mrhoads14@outlook.com>
2026-02-14 22:47:34 -0500

    implement /users/register and /users/login

---
+++ api/users.js
@@ -0,0 +1,33 @@
+import express from "express";
+import requireBody from "../middleware/requireBody.js";
+import { createUser, getUserByUsernamePassword } from "../db/queries/users.js";
+import { createToken } from "../utils/jwt.js";
+
+const router = express.Router();
+
+
+router.post(
+  "/register",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    const user = await createUser(req.body);
+    const token = createToken({ id: user.id });
+    res.status(201).send(token);
+  }
+);
+
+
+router.post(
+  "/login",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    const user = await getUserByUsernamePassword(req.body);
+    if (!user) {
+      res.status(401).send("Invalid username and/or password");
+    }
+    const token = createToken({ id: user.id });
+    res.send(token);
+  }
+);
+
+export default router;
--- db/queries/users.js
+++ db/queries/users.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import db from "#db/client";
 
 
-export const createUser = async (username, password) => {
+export const createUser = async ({ username, password }) => {
   const sql = `
   INSERT INTO users (username, password)
   VALUES ($1, $2)
@@ -14,4 +14,27 @@ export const createUser = async (username, password) => {
 
   const { rows: [user] } = await db.query(sql, [username, hashedPassword]);
   return user;
+}
+
+
+
+export const getUserByUsernamePassword = async ({ username, password }) => {
+  const sql = `
+  SELECT * FROM users WHERE username = $1;
+  `;
+  try {
+    const { rows: [user] } = await db.query(sql, [username]);
+    if (!user) {
+      return null;
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return null;
+    }
+
+    return user;
+  } catch (err) {
+    console.error('error in query: ', err);
+    throw err;
+  }
 }
